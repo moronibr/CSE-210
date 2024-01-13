fb7606619ed1a8bf612424c0b2838a76dbe4797c
moronibr <moroni.ramos@gmail.com>
2024-01-13 08:09:22 -0300

    prove

---
+++ prove/week01/MyConsoleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+int x;
+x = 5;
+Console.WriteLine(x);
+
+
+int y = 10;
+Console.WriteLine(y);
+
+Console.WriteLine("Hello World!");
+
+
+Console.WriteLine("What is your name? ");
+
+string name = Console.ReadLine()!;
+
+Console.WriteLine($"Hello {name}!");
+
+
+
+Console.WriteLine("Type a number");
+if (int.TryParse(Console.ReadLine(), out int number))
+if (number > 0)
+{
+    Console.WriteLine("Your number is positive");
+}
+else if (number == 0)
+{
+    Console.WriteLine("Your number is 0");
+}
+else 
+{
+    Console.WriteLine("Your number is negative");
+}
+
+int count = 0;
+while (count < 11)
+{
+    Console.WriteLine(count);
+    count++;
+}
+
+List<string> names = new List<string>();
+
+Console.WriteLine("Write your and your siblings names");
+
+names.Add(Console.ReadLine()!);
+
+foreach (var i in names)
+        {
+            Console.WriteLine($"The names are {i}");
+        }
+
+
+static void DisplayMessage()
+{
+    Console.WriteLine("Hello world!");
+}
+
+DisplayMessage();
