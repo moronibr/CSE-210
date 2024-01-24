f07212f920c4054f48db2f4016cc468d5ed24261
moronibr <moroni.ramos@gmail.com>
2024-01-24 19:44:45 -0300

    learning

---
+++ prepare/learning03/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Account
+{
+    private int _balance;
+
+    public Account(int initialBalance)
+    {
+        _balance = initialBalance;
+    }
+
+    public int Balance
+    {
+        get { return _balance; }
+        set { _balance = value; }
+    }
+
+    public void Deposit(int amount)
+    {
+        _balance += amount;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Account savings = new Account(50);
+        savings.Deposit(100);
+        
+     
+        Console.WriteLine($"Saldo atual: {savings.Balance}");
+    }
+}
